b8484df5e56bd6e73c6999b32264b8f9af73e229
Abassmoshood <Abassmoshoodadekilekun@gmail.com>
2023-02-16 08:52:32 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Print a line of code using puts
+ * 
+ * Return: O
+*/
+ int main(void)
+ { 
+	 puts("\"Programming is like building a multilingualpuzzle");
+	 return (O)
+ }
