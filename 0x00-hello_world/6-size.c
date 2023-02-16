98e9117bff6984b3cb592b301a53bef8e5033ae9
Abassmoshood <Abassmoshoodadekilekun@gmail.com>
2023-02-16 15:08:52 -0800

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,7 @@
+include <stdio.h>
+/**
+ * main - Entry                                    * Return: Always 0
+ */
+int main(void)                                    {
+        printf("size of a char: %1 byte(s)\n",sizeo f(char));                                               printf("size of a int: %1 byte(s)\n",sizeo f(int))
+	printf("size of a long int: %1 byte(s)\n", sizeof (long int));
